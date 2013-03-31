62afc3cece2872deeb882dc05177942c848191b6
Uni Ishizaki <uni.ishizaki@gmail.com>
2013-03-31 11:57:03 +0900

    initial commit

---
+++ app/models/saving.rb
@@ -0,0 +1,3 @@
+class Saving < ActiveRecord::Base
+  attr_accessible :date, :price
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaveMoney::Application.config.secret_token = '34549ff906a9c00c09a2b46d05fdc665582781ac8719a4fdd343a9b1721bc307fe88e6ff3e8dc5688cf623f744e851aec75180d8cc9a628c469dbd44ace67837'
